33afd93ba8e20311c04ae7753346019dd5d7c4f8
PanchoMrlda <fco.moraledac@gmail.com>
2019-09-10 19:00:26 +0200

    Implementing session options

---
+++ views/layouts/sessionOptions.php
@@ -0,0 +1,5 @@
+<section id="login-form" class="session-options">
+  <span class="session-option available"><i class="material-icons">person</i> Available</span>
+  <span class="session-option busy"><i class="material-icons">person</i> Busy</span>
+  <span class="session-option offline"><i class="material-icons">person</i> Offline</span>
+</section>
