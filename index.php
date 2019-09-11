e7c8f3d09f27c9a127902b03ed18d50e82ead40a
PanchoMrlda <fco.moraledac@gmail.com>
2019-09-11 18:19:16 +0200

    Refactoring index routes

--- index.php
+++ index.php
@@ -1,14 +1,8 @@
 <?php
 
 session_start();
-
-if (isset($_POST['start'])) {
-  $_SESSION['id'] = rand(1, 5);
-  include "views/welcome.php";
-} else if (isset($_POST['close'])) {
-  session_unset();
-  session_destroy();
-  include "views/welcome.php";
+if ($_POST['goTo'] == 'contact') {
+  include "views/contact.php";
 } else {
   include "views/welcome.php";
 }
