773907c4d165fd09a6998c5d2c4fe3c6e2804969
PanchoMrlda <fco.moraledac@gmail.com>
2019-09-11 10:43:23 +0200

    Fixing styles for session options

--- views/layouts/sessionOptions.php
+++ views/layouts/sessionOptions.php
@@ -1,5 +1,5 @@
 <section id="login-form" class="session-options">
-  <span class="session-option available"><i class="material-icons">person</i> Available</span>
-  <span class="session-option busy"><i class="material-icons">person</i> Busy</span>
-  <span class="session-option offline"><i class="material-icons">person</i> Offline</span>
+  <span class="session-option available"><i class="material-icons"  style="color:green">person</i>&emsp;Available</span>
+  <span class="session-option busy"><i class="material-icons"  style="color:red">person</i>&emsp;Busy</span>
+  <span class="session-option offline"><i class="material-icons"  style="color:black">person</i>&emsp;Offline</span>
 </section>
